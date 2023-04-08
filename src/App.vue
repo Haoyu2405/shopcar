a8a36b2d0c7751767d4a4e859481b61e525582b9
Wenyu520521 <2584107537@qq.com>
2023-04-08 11:17:43 +0800

    自定义头部

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <MyHeader backgroundColor="red" title="购物车案例"></MyHeader>
+    <div class="main">
+      <MyGoods></MyGoods>
+    </div>
+    <MyFooter></MyFooter>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+import MyGoods from './components/MyGoods.vue'
+import MyFooter from './components/MyFooter.vue'
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  components: {
+    MyHeader,
+    MyGoods,
+    MyFooter,
+  },
+}
+</script>
+<style scoped>
+.main {
+  padding-top: 45px;
+  padding-bottom: 50px;
+}
+</style>
